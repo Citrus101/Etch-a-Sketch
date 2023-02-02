9c26ef1396eae59f369668dec7d31d6f118c250c
Suman <Shumansswain@gmail.com>
2023-02-03 00:22:23 +0530

    Add a button which changes the grid size

--- jsfile.js
+++ jsfile.js
@@ -1,10 +1,58 @@
 const container = document.querySelector('.Container');
+// console.log(container.style.width);
+// const side = 10
 
-for(let i=0;i<16;i++){
-    for(let j=0;j<16;j++){   
-         const box = document.createElement("div")    
-        box.className = "grid";
-        container.appendChild(box);
-        // box.textContent = "nice"
-    }    
+function gridRemove(){
+    const grid = document.querySelectorAll('.grid');
+    grid.forEach((item) =>{
+        item.remove()
+    })
 }
+
+
+
+function gridSize(side){ 
+    
+    container.style.backgroundColor = "black";
+    for(let i=0;i<side;i++){
+        for(let j=0;j<side;j++){   
+            const box = document.createElement("div")    
+            box.className = "grid";
+            // box.textContent = i;
+            container.appendChild(box);
+            box.addEventListener('mouseenter',() => {
+                box.style.backgroundColor = 'blue';
+                
+
+            })
+            box.addEventListener('mouseleave',() => {
+            box.style.backgroundColor = 'white';
+
+            })
+            
+            box.style.width = `calc(100%/${side})`;
+            container.appendChild(box);
+
+
+            // box.textContent = "nice"
+        }    
+    }
+}
+
+const button = document.querySelector('button')
+// console.log(button)
+button.addEventListener('click',() => {
+    gridRemove();
+    size = parseInt(prompt("Enter Grid Size"));
+    gridSize(size);
+} )
+
+gridSize(20);
+// gridRemove();
+// gridSize(16);
+
+
+
+
+
+
